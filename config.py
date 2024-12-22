5db75c1d16bc7bcc7356880b4a28a9634cda1615
ZestyAscq <dan.pertu@gmail.com>
2024-12-22 15:51:39 +0000

    added gitignore file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = '1319665737412907109'
+CLIENT_SECRET = '8Sa2WtHUHid_MtgnZnE_TuEh3y7z9xLM'
 REDIRECT_URI = 'http://localhost:8000/callback'
 SCOPE = 'identify email'
